f8eb7f55da515c97b0cb1c8cae0685dff8b9c90e
jazielloureiro <jazielloureiro17@gmail.com>
2022-02-06 11:53:52 -0300

    Add structs and enum

--- rbtree.h
+++ rbtree.h
@@ -1,4 +1,18 @@
 #ifndef RBTREE_H
 #define RBTREE_H
 
+typedef enum {
+	RED, BLACK
+} rb_color;
+
+typedef struct Rb_node {
+	int key;
+	rb_color color;
+	Rb_node *parent, *left, *right;
+} rb_node;
+
+typedef struct {
+	rb_node *root, *nil;
+} rb_tree;
+
 #endif
