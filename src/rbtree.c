3cbac53d14cd627692eafa6f67fb5b6f6909aa95
jazielloureiro <jazielloureiro17@gmail.com>
2022-02-06 13:06:08 -0300

    Add left rotate

--- src/rbtree.c
+++ src/rbtree.c
@@ -1,1 +1,21 @@
 #include "rbtree.h"
+
+void left_rotate(rb_tree *tree, rb_node *x) {
+	rb_node *y = x->right;
+	x->right = y->left;
+
+	if(y->left != tree->nil)
+		y->left->parent = x;
+
+	y->parent = x->parent;
+
+	if(x->parent == tree->nil)
+		tree->root = y;
+	else if(x == x->parent->left)
+		x->parent->left = y;
+	else
+		x->parent->right = y;
+
+	y->left = x;
+	x->parent = y;
+}
